e6d23ac1ff0644a4f125e6b8ea4074282d5e529e
Connor Jones <122312244+Cojo99@users.noreply.github.com>
2023-08-06 16:21:06 -0600

    Update CootieMain.java

--- Cootie Catcher/CootieMain.java
+++ Cootie Catcher/CootieMain.java
@@ -1,3 +1,10 @@
+//Created by Connor Jones in 07/22 for an Intro to Programming class
+// at Weber State University.
+// Directions for the project were provided by Professor Kim Murphy.
+//The purpose of this project is to mimick the game played by folding
+// a piece of paper in such a way that there are different answers beneath different
+// flaps with numbers and colors written on them.
+//I used basic classes and methods to implememnt this project.
 import java.util.*;
 
 public class CootieMain
